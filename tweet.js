41e4b7be27ad7f8aadd1c1bd2f83ee6a5fd3a453
Harriet Ayugi <clerryha@gmail.com>
2020-12-07 12:26:43 +0300

    configure and setup playright with twetter test

---
+++ tweet.js
@@ -0,0 +1,16 @@
+(async () => {
+  const { chromium } = require("playwright");
+  const browser = await chromium.launch({ headless: false });
+  //   const browser = await chromium.launch();
+  const page = await browser.newPage();
+  await page.goto("https://twitter.com/login");
+  await page.fill('input[type="text"]', "<username>");
+  await page.fill('input[type="password"]', "<password>");
+  page.click('div[data-testid="LoginForm_Login_Button"]');
+  await page.fill(
+    ".public-DraftEditor-content",
+    "This is a test tweet using Microsoft Playwright"
+  );
+  page.click('div[data-testid="tweetButton"]');
+  await browser.close();
+})();
