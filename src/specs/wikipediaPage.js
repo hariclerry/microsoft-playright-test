9acb57f82eb97cd3a6275220aa2042394f05019a
Harriet Ayugi <clerryha@gmail.com>
2020-12-22 09:40:00 +0300

    add screenshot tests

--- src/specs/wikipediaPage.js
+++ src/specs/wikipediaPage.js
@@ -1,4 +1,7 @@
 const { chromium } = require("playwright");
+const path = require( 'path' )
+
+const LOGPATH = process.env.LOG_DIR || path.resolve(__dirname, "..", "log");
 
 describe("Wikipedia Home Page", () => {
   let response, browser, page;
@@ -9,12 +12,14 @@ describe("Wikipedia Home Page", () => {
     response = await page.goto("https://www.wikipedia.org/");
   });
 
+  beforeEach(async () => {
+    	await page.screenshot({
+        path: `${LOGPATH}/screenshot.png`,
+      });
+  })
+
   afterAll(async () => {
     await browser.close();
-  });
-
-  it("test if page loads successfully", async () => {
-    expect(response.status()).toBe(200);
   });
 
   it("should check if page sub title is correct", async () => {
